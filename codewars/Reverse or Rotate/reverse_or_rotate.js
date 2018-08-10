421de962e97d482e3099d870a5c18524eaa0da63
Tom Cariello <tomcariello@gmail.com>
2018-08-10 07:14:03 -0400

    updated answer for reverse or rotate

--- codewars/Reverse or Rotate/reverse_or_rotate.js
+++ codewars/Reverse or Rotate/reverse_or_rotate.js
@@ -19,21 +19,11 @@
 //      revrot("123456779", 0) --> "" 
 //      revrot("563000655734469485", 4) --> "0365065073456944"
 
-//NOTES
-// This feels pretty inefficient, particularly the nested FOR loops. But it works well enough to pass the tests.
-// I am not checking to see if sz > str.length which feels like a flaw. But Codewars doesn't test that case, so it passes.
-
-
-let chunkArray = []; //store the values after the string is broken into sz sized chunks
-let marker = 0; //store position of the position to start the slice. Used to facilitate splitting str into chunks
-let answer = "";
-
 function revrot(str, sz) {
 
-    //reset lets
+    //declars vars
     let chunkArray = [];
     let marker = 0; 
-    let answer = "";
 
     if (sz <= 0 || str == "") {
         return "";
@@ -44,17 +34,14 @@ function revrot(str, sz) {
             marker += sz;
         }
     }
-     
+    
     //Loop through chunks
     for (j=0; j < chunkArray.length; j++) {
-                
-        let chunkSum = 0;
-        
-        //Loop through each digit of each chunk
-        for (k=0; k < chunkArray[j].length; k++) {
-            //obtain the sum of the cubes of the digits in this chunk
-            chunkSum += Math.pow(chunkArray[j][k], 3)
-        }
+    
+        //Split the array into individual digits .then
+        //Use reduce to iterator through the new array of digits and cube each digit
+        let chunkSum = chunkArray[j].split('').reduce( (accumulater, digit) => accumulater + Math.pow(digit, 3),0 );
+
         //if even REVERSE
         if (chunkSum % 2 == 0) {
             chunkArray[j] = reverse(chunkArray[j]);
@@ -63,33 +50,16 @@ function revrot(str, sz) {
         }
     }
 
-    for (m=0; m < chunkArray.length; m++) {
-        answer += chunkArray[m];
-    }
-
-    //Return string
-    return answer;
+    //Return joined chunkArray
+    return chunkArray.join('');
 }
 
 function reverse(chunk) {
-
-    let revChunk = ""
-
-    for (i=chunk.length; i > 0; i--) {
-        revChunk += chunk[i - 1];
-    }
-    return revChunk;
+    //split chunk .then reverse the elements .then join the elements back into 1 string of digits without a delimitr
+    return chunk.split('').reverse().join('');
 }
 
 function rotate(chunk) {
-    //Store the 1st digit
-    let firstDigit = chunk.slice(0,1);
-
-    //Remove the 1st digit from the chunk
-    chunk = chunk.substr(1);
-
-    //Append the first digit to the end of chunk
-    chunk += firstDigit
-
-    return chunk;
+    //array without the first digit + the first digit to rotate the first position to the end of the array
+    return chunk.slice(1) + chunk.slice(0,1);
 }
