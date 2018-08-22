ce877c107b58ec61d10723c8430027b54e3ece5d
Tom Cariello <tomcariello@gmail.com>
2018-08-22 19:26:18 -0400

    solved Euler3

--- project_euler/3/euler3.js
+++ project_euler/3/euler3.js
@@ -3,37 +3,20 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 
 // What is the largest prime factor of the number 600851475143 ?
-
+// 600,851,475,143 - 600 Billion
 const lookupValue = 600851475143;
 
 let findLargestPrime = (val) => {
 
     var largestprime = 1;
 
-    // Eliminate as many items as we can to improve performance
-    // Things we can eliminate:
-    // Eliminate even numbers
-    // Eliminate numbers greater than 1/2 of val + 1
-    
-    // Eliminate multiples of 5 if val doesn't end in 5 or 0. 
-    // Determine the last digit of the number
-    let lastDigit = lookupValue.toString().charAt(lookupValue.toString().length-1);
-
-    if (lastDigit != 0 && lastDigit != 5) {
-        //eliminate 5 from the mix
-    }
-
-    // Eliminate multiples of 3 if the sum of the numbers is not divisible by 3
-    let sumOfDigits = lookupValue.toString().split('').reduce(function(accumulator, currentValue, currentIndex, array) {
-        return parseInt(accumulator) + parseInt(currentValue);
-      });
+    // Set the limit on the numbers to check up to
+    let limit = Math.sqrt(lookupValue);
 
     //Loop through the odd numbers 
-    for (var i=3; i < val / 2 + 1; i+=2) {
-        console.log(`checking if ${val} is divisible by ${i}`);
+    for (var i=3; i < limit; i+=2) {
         //If this number is evenly divisible by i
         if (val % i == 0) {
-            console.log(`checking if ${i} is prime...`);
             //check if i is prime
             if (isPrime(i) == true) {
                 console.log(`It is! The largest prime divisor for ${val} so far is ${i}`)
@@ -50,11 +33,16 @@ let isPrime = (num) => {
     if (num == 1) {
          return true;
     }
-    
-    for (var i=2; i <= num-1; i++) {
+    if (num % 2 == 0) {
+        return true;
+   }
+
+    for (var i=3; i <= num / 2 -1; i += 2) {
         if (num % i == 0) {
             return false;
         }
     }
     return true;
 }
+
+findLargestPrime(lookupValue);
