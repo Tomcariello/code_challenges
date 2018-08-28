2a22c43e493f057d38e3049ea19872b0cbaa4b94
Tom Cariello <tomcariello@gmail.com>
2018-08-27 22:33:18 -0400

    Finished Codewars  A Gift Well Spent

---
+++ codewars/a_gift_well_spent/a_gift_well_spent.js
@@ -0,0 +1,43 @@
+// Note: This kata has been inspired by GCJ 2010's "Store credit", where one also has to parse the actual input. 
+// If you solved this kata, try that one too. Note that GCJ's version always has a solution, whereas this kata might not.
+
+// Story
+// You got a gift card for your local store. It has some credit you can use to buy things, but it may be used only for up to two items.
+// Any credit you don't use is lost. You want something for a friend and yourself. 
+// Therefore, you want to buy two items which add up the entire gift card value.
+
+// Task
+// You will get the value of the gift card c and a finite list of item values. 
+// You should return a pair of indices that correspond to values that add up to c:
+
+// buy(2,[1,1])       = [0,1]
+// buy(3,[1,1])       = null
+// buy(5,[5,2,3,4,5]) = [1,2]
+// The indices start at 0. The first index should always be smaller than the second index. 
+// If there are multiple solutions, return the minimum (lexicographically):
+
+// buy(5,[1,2,3,4,5]) = [0,3] // the values at [1,2] also adds up to five, but [0,3] < [1,2]
+
+
+var buy = function(x, arr){
+    // If array is too short, return null
+    if (arr.length <2 ) {
+        return null;    
+    } else {
+        // Begin looping through the array
+        for (i=0; i<arr.length; i++) {
+            // Begin next loop. Iterate through the array from the next position
+            for (j=i+1; j<arr.length; j++) {
+                // Check it total is equal to target value
+                if (arr[i] + arr[j] == x) {
+                    // It's a match! Add the indexes in an array
+                    return [i,j];
+                }
+            }
+        }
+    }
+    // return null if nothing found
+    return null;
+};
+
+
